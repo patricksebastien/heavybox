6a951efed5ae28e820a492181906b9eb5ca4baee
patrick sebastien <patrick@11h11.com>
2013-01-17 12:55:10 -0500

    ...

--- control/heavybox.js
+++ control/heavybox.js
@@ -8,7 +8,7 @@ pages = [
 {
     "name": "infoText",
     "type": "Label",
-    "x": 0.05, "y": 0,
+    "x": 0.01, "y": 0,
     "width": 0.1, "height": 0.1,
     "value": "RACK 1",
 	"align": "left",
@@ -16,9 +16,17 @@ pages = [
 {
     "name" : "mySlider",
     "type" : "Slider",
-    "x" : 0.05, "y" : 0.2,
-    "width" : .25, "height" : .75,
+    "x" : 0.01, "y" : 0.1,
+    "width" : .25, "height" : .55,
     "address" : "/vol",
+},
+{
+     "name" : "myMultiSlider",
+     "type" : "MultiSlider",
+     "x" : 0.03, "y" : 0,
+     "width" : .8, "height" : .5,
+     "numberOfSliders" : 6,
+     "isVertical" : true,
 },
 {
     "name": "refresh",
