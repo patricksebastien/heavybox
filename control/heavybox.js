28c90295a29b1ac8fe9d832cbdee5c2d2aef815b
psc <psc@pscrog.(none)>
2013-01-17 22:38:22 -0500

    ...

--- control/heavybox.js
+++ control/heavybox.js
@@ -1,6 +1,78 @@
 loadedInterfaceName = "heavybox";
 interfaceOrientation = "landscape";
 
+
+
+constants = [
+
+{
+    "name": "page1Btn",
+    "type": "Button",
+    "bounds": [0,.9,.2,.1], 
+    "label": "1",
+    "mode": "contact",    
+    "ontouchstart": "control.changePage(0);",
+    "stroke": "#aaa",    
+},
+{
+    "name": "page2Btn",
+    "type": "Button",
+    "bounds": [.2,.9,.2,.1], 
+    "label": "2",
+    "mode": "contact",    
+    "ontouchstart": "control.changePage(1);",
+    "stroke": "#aaa",    
+},
+{
+    "name": "page3Btn",
+    "type": "Button",
+    "bounds": [.4,.9,.2,.1], 
+    "label": "3",
+    "mode": "contact",
+    "ontouchstart": "control.changePage(2);",
+    "stroke": "#aaa",
+},
+{
+    "name": "nextBtn",
+    "type": "Button",
+    "bounds": [0,.9,.2,.1], 
+    "label": "<-",
+    "mode": "contact",    
+    "ontouchstart": "control.changePage('previous');",
+    "stroke": "#aaa",    
+},
+{
+    "name": "page2Btn",
+    "type": "Button",
+    "bounds": [.2,.9,.2,.1], 
+    "label": "->",
+    "mode": "contact",    
+    "ontouchstart": "control.changePage('next');",
+    "stroke": "#aaa",    
+},
+{
+    "name": "refresh",
+    "type": "Button",
+    "bounds": [.6, .9, .2, .1],
+    "startingValue": 0,
+    "isLocal": true,
+    "mode": "contact",
+    "ontouchstart": "interfaceManager.refreshInterface()",
+    "stroke": "#aaa",
+    "label": "refresh",
+},
+{
+    "name": "tabButton",
+    "type": "Button",
+    "bounds": [.8, .9, .2, .1],
+    "mode": "toggle",
+    "stroke": "#aaa",
+    "isLocal": true,
+    "ontouchstart": "if(this.value == this.max) { control.showToolbar(); } else { control.hideToolbar(); }",
+    "label": "menu",
+},
+];
+
 pages = [
 
 [
@@ -30,29 +102,18 @@ pages = [
      "numberOfSliders" : 3,
      "isVertical" : true,
 },
-{
-    "name": "refresh",
-    "type": "Button",
-    "bounds": [.6, .85, .2, .1],
-    "startingValue": 0,
-    "isLocal": true,
-    "mode": "contact",
-    "ontouchstart": "interfaceManager.refreshInterface()",
-    "stroke": "#aaa",
-    "label": "refresh",
-},
-{
-    "name": "tabButton",
-    "type": "Button",
-    "bounds": [.8, .85, .2, .1],
-    "mode": "toggle",
-    "stroke": "#aaa",
-    "isLocal": true,
-    "ontouchstart": "if(this.value == this.max) { control.showToolbar(); } else { control.hideToolbar(); }",
-    "label": "menu",
-},
-]
+],
+/********** PAGE 2 *************/
+[
+    {
+        "name":"page2Label",
+        "type":"Label",
+        "value":"PAGE 2",
+        "size": "40",   
+        "bounds": [0,0,1,.5]             
+    }
 
+],
 
 
 
