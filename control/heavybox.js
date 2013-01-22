9d0a05216d318bf5b94ba4945a24a44aa6c488e9
patricksebastien <patrick@11h11.com>
2013-01-22 12:56:09 -0500

    ...

--- control/heavybox.js
+++ control/heavybox.js
@@ -14,11 +14,19 @@ oscManager.delegate = {
                 control.changePage(args[0]);
                 break;
 			case "/w":
-				var w = control.getWidgetWithName(arguments[0]);
+				var w = control.getWidgetWithName(arguments[1]);
 				w.setColors(["#ff0", "#f30", "#3ff"]);
 				break;
 			case "/x":
 				var w = control.getWidgetWithName("tuning");
+				w.setColors(["#ff0", "#f30", "#3ff"]);
+				break;
+			case "/y":
+				var w = control.getWidgetWithName(arguments[2]);
+				w.setColors(["#ff0", "#f30", "#3ff"]);
+				break;
+			case "/z":
+				var w = control.getWidgetWithName(arguments[3]);
 				w.setColors(["#ff0", "#f30", "#3ff"]);
 				break;
 			default:
