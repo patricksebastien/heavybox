32ea46d33ea64c204174dc6c7bcd577b8269271f
patricksebastien <patrick@11h11.com>
2013-01-24 21:15:20 -0500

    ...

--- control/heavybox.js
+++ control/heavybox.js
@@ -37,7 +37,7 @@ oscManager.delegate = {
 			case "/t":
 				var w = control.getWidgetWithName(arguments[2]);
 				var f = arguments[2];
-				var v = f + "test";
+				var v = f + arguments[3];
 				w.setValue(v);
 				break;
 			case "/ta":
